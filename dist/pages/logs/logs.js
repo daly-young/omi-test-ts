a6e9ea99ac667a0345b2194a9cfa37f8df60621c
yangjun <yangjun@aixuedai.com>
2020-03-01 16:45:14 +0800

    feat: 配置

---
+++ dist/pages/logs/logs.js
@@ -0,0 +1,14 @@
+//logs.js
+import { formatTime } from '../../utils/util';
+Page({
+    data: {
+        logs: []
+    },
+    onLoad() {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map((log) => {
+                return formatTime(new Date(log));
+            })
+        });
+    },
+});
